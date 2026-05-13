04a272891a54667df6da54842795375d4d25d8a4
egeltaa <egemensetol08@gmail.com>
2026-05-13 22:48:52 +0300

    Added Fotos

--- Harita.js
+++ Harita.js
@@ -8,97 +8,97 @@ const yerler = [
         id: 1,
         ad: "Şair Nedim Caddesi",
         koordinat: [41.044008, 29.001633],
-        resim: "",
+        resim: "Foto\\Sair Nedim.jpeg",
         link: "https://maps.app.goo.gl/R7eLG8ddvSAiadjw9",
-        aciklama: "."
+        aciklama: "İstanbul'un tarihli sokaklarından biri."
     },
     {
         id: 2,
         ad: "Cemal Süreya Sokak",
         koordinat: [40.988698, 29.022150],
-        resim: "",
+        resim: "Foto\\Cemal Süreya.jpeg",
         link: "https://maps.app.goo.gl/29rp5uA48WMgJuAB9", 
-        aciklama: "."
+        aciklama: "Edebiyat tarihinin önemli bir mekânı."
     },
     {
         id: 3,
         ad: "Can Yücel Sokak",
         koordinat: [41.030207, 29.037386],
-        resim: "",
+        resim: "Foto\\Can Yücel Sokak.jpeg",
         link: "https://maps.app.goo.gl/zrXcUtAaceLnVHeP8",
-        aciklama: "."
+        aciklama: "İstanbul'un sanat ve kültür merkezi."
     },
     {
         id: 4,
         ad: "Fazıl Hüsnü Dağlarca Sokak",
         koordinat: [40.986343, 29.025519],
-        resim: "",
+        resim: "Foto\\Dağlarca.jpeg",
         link: "https://maps.app.goo.gl/qcnJngEQ9aq44eT8A",
-        aciklama: "."
+        aciklama: "Şair Dağlarca'nın yaşadığı sokak."
     },
     {
         id: 5,
         ad: "Melih Cevdet Anday Sokak",
         koordinat: [40.989181, 29.061434],
-        resim: "",
+        resim: "Foto\\Melih Cevdet Anday.jpeg",
         link: "https://maps.app.goo.gl/PknRQqF2tdBMTFPR7",
-        aciklama: "."
+        aciklama: "Meşhur şair Anday'ın izleri."
     },
     {
         id: 6,
         ad: "Mehmet Akif Ersoy Caddesi",
         koordinat: [41.036914, 29.065755],
-        resim: "",
+        resim: "Foto\\Mehmet Akif.jpeg",
         link: "https://maps.app.goo.gl/RBnz5gqEDT9LmBsH8",
-        aciklama: "."
+        aciklama: "Milli Şair Ersoy'un anısına adanmış cadde."
     },
     {
         id: 7,
         ad: "Necip Fazıl Kısakürek Sokak",
         koordinat: [41.037376, 29.089138],
-        resim: "",
+        resim: "Foto\\NecipFazıl.jpeg",
         link: "https://maps.app.goo.gl/8v5p2qQ4v6dD6",
-        aciklama: "."
+        aciklama: "Mübarek Sokak'ın yazarı Kısakürek'in adıyla anılıyor."
     },
     {
         id: 8,
-        ad: "Oğuz Atay'ın Tutunamayanları yazdığı ev",
+        ad: "Oğuz Atay'ın Evi",
         koordinat: [41.031901, 28.980064],
-        resim: "",
+        resim: "Foto\\OğuzAtay.jpg",
         link: "https://maps.app.goo.gl/doRUYd4m89Dm7NSP7",
-        aciklama: "."
+        aciklama: "Ünlü eseri 'Tutunamayanlar'ın yazıldığı ev."
     },
     {
         id: 9,
         ad: "Sadri Alışık Sokak",
         koordinat: [41.034043, 28.981196],
-        resim: "",
+        resim: "Foto\\SadriAlışık.png",
         link: "https://maps.app.goo.gl/a21fAWdLwMHMKVua6",
-        aciklama: "."
+        aciklama: "Ünlü oyuncunun adıyla anılıyor."
     },
     {
         id: 10,
         ad: "Orhan Kemal Müzesi",
         koordinat: [41.03055, 28.98357],
-        resim: "",
+        resim: "Foto\\OrhanKemal.png",
         link: "https://maps.app.goo.gl/QCBdr64bdsTNL8xn9",
-        aciklama: "."
+        aciklama: "Ünlü yazarın yaşadığı ve müzeleştirilmiş evi."
     },
     {
         id: 11,
         ad: "Yahya Kemal Parkı",
         koordinat: [41.0505, 29.0068],
-        resim: "",
+        resim: "Foto\\Park.jpeg",
         link: "https://maps.app.goo.gl/nkcfcjfzy2pmPSmE9",
-        aciklama: "."
+        aciklama: "Şair Yahya Kemal'e adanmış park."
     },
     {
         id: 12,
         ad: "Şair Leyla Sokağı",
         koordinat: [41.043219, 29.004534],
-        resim: "",
+        resim: "Foto\\Şair Leyla.jpeg",
         link: "https://maps.app.goo.gl/mJmGr8FGZ7PZgtH1A",
-        aciklama: "."
+        aciklama: "İstanbul'un edebiyat tarihi içinde önemli bir mekân."
     }
 ];
 
